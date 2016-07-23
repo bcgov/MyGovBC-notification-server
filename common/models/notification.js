33013fb030fbe5492327b0774c1e9bcb2df1e121
f-w <fred.wen@gov.bc.ca>
2016-07-22 18:02:05 -0700

    added Operation hook - access

--- common/models/notification.js
+++ common/models/notification.js
@@ -1,9 +1,16 @@
-module.exports = function(Notification) {
+module.exports = function (Notification) {
   var isStatic = true
   Notification.disableRemoteMethod('findOne', isStatic)
   Notification.disableRemoteMethod('createChangeStream', isStatic)
   Notification.disableRemoteMethod('exists', isStatic)
   Notification.disableRemoteMethod('updateAll', isStatic)
-  Notification.disableRemoteMethod('create', isStatic)
+  //Notification.disableRemoteMethod('create', isStatic)
   Notification.disableRemoteMethod('count', isStatic)
+  Notification.disableRemoteMethod('upsert', isStatic)
+  Notification.observe('access', function (ctx, next) {
+    var httpCtx = require('loopback').getCurrentContext();
+    ctx.query.where = ctx.query.where || {}
+    ctx.query.where.userId = httpCtx.active.http.req.get('sm_user') || httpCtx.active.http.req.get('smgov_userdisplayname') || 'unknown'
+    next()
+  })
 };
