558cb746b4cb1084ce3e86c85573d0df2b059061
f-w <fred.wen@gov.bc.ca>
2016-08-05 14:47:14 -0700

    filter out expired notifications.

--- common/models/notification.js
+++ common/models/notification.js
@@ -10,9 +10,29 @@ module.exports = function (Notification) {
   Notification.observe('access', function (ctx, next) {
     var httpCtx = require('loopback').getCurrentContext();
     ctx.query.where = ctx.query.where || {}
-    ctx.query.where.userId = httpCtx.active.http.req.get('sm_user') || httpCtx.active.http.req.get('smgov_userdisplayname') || 'unknown'
+    if (httpCtx.active.http.req.get('sm_user') || httpCtx.active.http.req.get('smgov_userdisplayname')) {
+      ctx.query.where.or = []
+      ctx.query.where.or.push({
+        isBroadcast: true
+      })
+      ctx.query.where.or.push({
+        userId: httpCtx.active.http.req.get('sm_user') || httpCtx.active.http.req.get('smgov_userdisplayname')
+      })
+    }
     next()
   })
+  Notification.afterRemote('find', function (ctx, res, next) {
+    if (!res) {
+      return
+    }
+    ctx.result = res.filter(function (e, i) {
+      if (!e.validTill) return true
+      if (Date.parse(e.validTill) > new Date()) return true
+      return false
+    })
+    next()
+  })
+
   Notification.beforeRemote('create', function (ctx, unused, next) {
     if (ctx.req.get('sm_user') || ctx.req.get('smgov_userdisplayname')) {
       var error = new Error('Unauthorized')
