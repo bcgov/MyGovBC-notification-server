0a15f78ba7f3b5a8fe48eca60faf956862fbf345
f-w <fred.wen@gov.bc.ca>
2016-08-22 16:18:10 -0700

    generate RSA key pair at boot if needed.

---
+++ server/boot/rsa.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const path = require('path')
+var NodeRSA = require('node-rsa')
+
+module.exports = function (app, cb) {
+  /*
+   * The `app` object provides access to a variety of LoopBack resources such as
+   * models (e.g. `app.models.YourModelName`) or data sources (e.g.
+   * `app.datasources.YourDataSource`). See
+   * http://docs.strongloop.com/display/public/LB/Working+with+LoopBack+objects
+   * for more info.
+   */
+  var privateKeyFilePath = path.resolve(__dirname, '../id_rsa')
+  var publicKeyFilePath = path.resolve(__dirname, '../id_rsa.pub')
+  fs.readFile(privateKeyFilePath, 'utf8', (err, data) => {
+    var key = new NodeRSA()
+    if (!err) {
+      key.importKey(data, 'private')
+      module.exports.key = key
+      return cb()
+    }
+    key.generateKeyPair()
+    module.exports.key = key
+    var privateKey = key.exportKey('private')
+    var publicKey = key.exportKey('public')
+    fs.writeFile(privateKeyFilePath, privateKey, (err) => {
+      if (err) throw err
+      fs.writeFile(publicKeyFilePath, publicKey, (err) => {
+        if (err) throw err
+        cb()
+      })
+    })
+  })
+}
