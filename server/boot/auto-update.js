da3a810b7d523c4cadb497ce1404a8024f1b7f93
f-w <fred.wen@gov.bc.ca>
2017-07-12 16:05:14 -0700

    it caused master to fail.

--- server/boot/auto-update.js
+++ server/boot/auto-update.js
@@ -1,28 +1,28 @@
 module.exports = function (app, cb) {
-  if (process.env.NOTIFYBC_NODE_ROLE !== 'slave') {
-    const pjson = require('../../package.json')
-    const semver = require('semver')
-    const targetVersion = pjson.dbSchemaVersion
-    app.models.Configuration.findOrCreate({where: {name: 'dbSchemaVersion'}}, {
-      name: "dbSchemaVersion",
-      value: "0.0.0"
-    }, function (err, data) {
-      const currentVersion = data.value
-      if (semver.major(targetVersion) === semver.major(currentVersion) && semver.minor(targetVersion) > semver.minor(currentVersion)) {
-        app.dataSources.db.autoupdate(function (err, result) {
-          if (err) {
-            throw err
-          }
-          else {
-            data.updateAttribute('value', targetVersion)
-            return cb()
-          }
-        })
-      }
-      else {
-        return cb()
-      }
-    })
+  if (process.env.NOTIFYBC_NODE_ROLE === 'slave') {
+    return process.nextTick(cb)
   }
-  cb()
+  const pjson = require('../../package.json')
+  const semver = require('semver')
+  const targetVersion = pjson.dbSchemaVersion
+  app.models.Configuration.findOrCreate({where: {name: 'dbSchemaVersion'}}, {
+    name: "dbSchemaVersion",
+    value: "0.0.0"
+  }, function (err, data) {
+    const currentVersion = data.value
+    if (semver.major(targetVersion) === semver.major(currentVersion) && semver.minor(targetVersion) > semver.minor(currentVersion)) {
+      app.dataSources.db.autoupdate(function (err, result) {
+        if (err) {
+          throw err
+        }
+        else {
+          data.updateAttribute('value', targetVersion)
+          return cb()
+        }
+      })
+    }
+    else {
+      return cb()
+    }
+  })
 }
