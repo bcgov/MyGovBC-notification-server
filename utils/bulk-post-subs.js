2e9bd67119137b40781d55739f546bbc5243f288
f-w <fred.wen@gov.bc.ca>
2017-07-26 15:54:08 -0700

    #20 added load testing script

---
+++ utils/bulk-post-subs.js
@@ -0,0 +1,50 @@
+'use strict'
+let request = require('request')
+var parallelLimit = require('async/parallelLimit')
+if (process.argv.length < 3) {
+  process.process.exit(1)
+}
+let tasks = []
+let i = 0
+while (i < (process.argv[4] || 1000)) {
+  let index = i
+  tasks.push(function(cb) {
+    let options = {
+      uri: process.argv[2] + '/subscriptions',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      json: {
+        serviceName: 'load',
+        channel: 'email',
+        state: 'confirmed',
+        index: index,
+        confirmationRequest: {
+          sendRequest: false
+        },
+        userChannelId: process.argv[3],
+        broadcastPushNotificationFilter:
+          "contains_ci(title,'vancouver') || contains_ci(title,'victoria')"
+      }
+    }
+    request.post(options, (err, data) => {
+      if (!err && data.statusCode !== 200) {
+        err = data.statusCode
+      }
+      try {
+        console.log(data.body.index)
+      } catch (ex) {}
+      cb(err, data)
+    })
+  })
+  i++
+}
+
+parallelLimit(tasks, 50, function(error, data) {
+  if (error) {
+    console.log(error)
+  } else if (data) {
+    console.log('total count ' + data.length)
+    process.exit(0)
+  }
+})
