1c9363a51456064a940883d65331b52a1de3713c
f-w <fred.wen@gov.bc.ca>
2017-07-28 18:55:02 -0700

    added curl script to send notificaiton; postman seems sending multiple requests

---
+++ utils/curl-ntf.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+time curl -X POST http://localhost:3000/api/notifications \
+ -H 'Content-Type: application/json' \
+ -d @- << EOF
+{
+   "serviceName": "load10",
+   "message": {
+       "from": "$1",
+       "subject": "{title}",
+       "textBody": "{description}",
+       "htmlBody": "{description}<p><a href='{link}'>link</a><a href='{unsubscription_url}'>nsubscription</a></p>"
+   },
+   "channel": "email",
+   "isBroadcast": true,
+   "data": {
+       "title": "Despite unpopularity, Victoria fadsasdfasd",
+       "description": "Lorem ipsum dolor sit amet, facete debitis dolores nam eu, nemore voluptatum interesset at mel. Duo et legimus vituperata, mei adipisci prodesset conclusionemque an. Mnesarchum adversarium eam eu, ad postea labore vituperatoribus eam. Dicam convenire vis ei, id vis quod luptatum. Expetenda consequat at quo, mel inermis volumus intellegam ut, mei vocibus inciderint ea. At error viris has.",
+       "pubDate": "2017-07-20T16:18:04.000Z",
+       "link": "http://foo.com",
+       "guid": "12356"
+   }
+}
+EOF
