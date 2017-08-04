0b0f2e4596fc5b8eaa9fcc9949f27ca8980a9ee8
f-w <fred.wen@gov.bc.ca>
2017-08-03 17:11:56 -0700

    js version of post notification

---
+++ utils/post-notification.js
@@ -0,0 +1,40 @@
+'use strict'
+let request = require('request')
+if (process.argv.length < 2) {
+  process.exit(1)
+}
+
+let options = {
+  uri: process.argv[2] + '/notifications',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  json: {
+    serviceName: process.argv[3] || 'load10',
+    message: {
+      from: process.argv[4] || 'no_reply@example.com',
+      subject: '{title}',
+      textBody: '{description}',
+      htmlBody:
+        "{description}<p><a href='{link}'>link</a><a href='{unsubscription_url}'>nsubscription</a></p>"
+    },
+    channel: 'email',
+    isBroadcast: true,
+    data: {
+      title: 'Despite unpopularity, Victoria fadsasdfasd',
+      description:
+        'Lorem ipsum dolor sit amet, facete debitis dolores nam eu, nemore voluptatum interesset at mel. Duo et legimus vituperata, mei adipisci prodesset conclusionemque an. Mnesarchum adversarium eam eu, ad postea labore vituperatoribus eam. Dicam convenire vis ei, id vis quod luptatum. Expetenda consequat at quo, mel inermis volumus intellegam ut, mei vocibus inciderint ea. At error viris has.',
+      pubDate: '2017-07-20T16:18:04.000Z',
+      link: 'http://foo.com',
+      guid: '12356'
+    }
+  }
+}
+request.post(options, (err, response, body) => {
+  if (err) {
+    console.error(err)
+  }
+  console.log('response.statusCode=' + response.statusCode)
+  console.log(body)
+  process.exit(0)
+})
