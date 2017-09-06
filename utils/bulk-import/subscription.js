323c2da61fd983907765539493b3c759d6661d34
f-w <fred.wen@gov.bc.ca>
2017-09-06 13:17:06 -0700

    #16: parse command args

--- utils/bulk-import/subscription.js
+++ utils/bulk-import/subscription.js
@@ -2,11 +2,35 @@
 const request = require('request')
 const csv = require('csvtojson')
 const queue = require('async/queue')
+const program = require('commander')
+const pjson = require('../../package.json')
+program
+  .version(pjson.version)
+  .description('bulk import subscriptions from csv file')
+  .usage('[options] <csv-file-name>')
+  .option(
+    '-a --api-url-prefix <api-url-prefix>',
+    'api url prefix. default to http://localhost:3000/api',
+    'http://localhost:3000/api'
+  )
+  .option(
+    '-c --concurrency <concurrency>',
+    'post request concurrency. default to 10',
+    parseInt,
+    10
+  )
+  .parse(process.argv)
+if (program.args.length !== 1) {
+  console.error('invalid arguments')
+  program.outputHelp()
+  process.exit(1)
+}
+
 let done = false,
   successCnt = 0
 let q = queue(function(task, cb) {
   let options = {
-    uri: (process.argv[3] || 'http://localhost:3000/api') + '/subscriptions',
+    uri: program.apiUrlPrefix + '/subscriptions',
     headers: {
       'Content-Type': 'application/json'
     },
@@ -23,7 +47,7 @@ let q = queue(function(task, cb) {
     }
     cb(err)
   })
-}, process.argv[4] || 10)
+}, program.concurrency)
 q.drain = function() {
   if (done) {
     console.log('success row count = ' + successCnt)
@@ -42,7 +66,7 @@ csv({
     }
   }
 })
-  .fromFile(process.argv[2])
+  .fromFile(program.args[0])
   .on('json', (jsonObj, rowIdx) => {
     q.push({
       jsonObj: jsonObj,
