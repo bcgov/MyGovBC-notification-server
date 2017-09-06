952215e07fdd77b58ad7c777e68e88022b8c4245
f-w <fred.wen@gov.bc.ca>
2017-09-06 10:38:53 -0700

    #16: uses queue to throttle request rate.

--- utils/bulk-import/subscription.js
+++ utils/bulk-import/subscription.js
@@ -1,6 +1,36 @@
 'use strict'
 const request = require('request')
 const csv = require('csvtojson')
+const queue = require('async/queue')
+let done = false,
+  successCnt = 0
+let q = queue(function(task, cb) {
+  let options = {
+    uri: (process.argv[3] || 'http://localhost:3000/api') + '/subscriptions',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    json: task.jsonObj
+  }
+  request.post(options, (err, data) => {
+    if (!err && data.statusCode !== 200) {
+      err = data.statusCode
+    }
+    if (err) {
+      console.error('error for row #' + task.rowIdx + ': ' + err)
+    } else {
+      successCnt++
+    }
+    cb(err)
+  })
+}, process.argv[4] || 10)
+q.drain = function() {
+  if (done) {
+    console.log('success row count = ' + successCnt)
+    process.exit(0)
+  }
+}
+
 csv({
   colParser: {
     'confirmationRequest.sendRequest': (item, head, resultRow, row, colIdx) => {
@@ -14,22 +44,12 @@ csv({
 })
   .fromFile(process.argv[2])
   .on('json', (jsonObj, rowIdx) => {
-    let options = {
-      uri: (process.argv[3] || 'http://localhost:3000/api') + '/subscriptions',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      json: jsonObj
-    }
-    request.post(options, (err, data) => {
-      if (!err && data.statusCode !== 200) {
-        err = data.statusCode
-      }
-      if (err) {
-        console.error('error for row #' + rowIdx + ': ' + err)
-      }
+    q.push({
+      jsonObj: jsonObj,
+      rowIdx: rowIdx
     })
   })
   .on('done', error => {
-    console.log('end')
+    error && console.error(error)
+    done = true
   })
