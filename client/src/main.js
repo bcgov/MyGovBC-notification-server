49064cf63061b8d2312f9d699a4a804444de120d
f-w <fred.wen@gov.bc.ca>
2017-10-12 16:45:48 -0700

    added http://vuematerial.io

--- client/src/main.js
+++ client/src/main.js
@@ -4,8 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-
 Vue.config.productionTip = false
+
+let VueMaterial = require('vue-material')
+Vue.use(VueMaterial)
 
 /* eslint-disable no-new */
 new Vue({
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import createLogger from 'vuex/dist/logger'
 
 Vue.use(Vuex)
 
-const debug = process.env.NODE_ENV !== 'production'
-
 export default new Vuex.Store({
-  state: {},
+  state: {
+    notifications: []
+  },
   mutations: {},
-  strict: debug,
-  plugins: debug ? [createLogger()] : []
+  strict: process.env.NODE_ENV !== 'production'
 })
