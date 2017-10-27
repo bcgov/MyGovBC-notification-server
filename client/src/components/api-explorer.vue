a8cbaea272fe0b3be339d577941c248d6816896d
f-w <fred.wen@gov.bc.ca>
2017-10-27 14:35:28 -0700

    enabled embedded api-explorer using iframe resizer

--- client/src/components/api-explorer.vue
+++ client/src/components/api-explorer.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-    <h6>API-Explorer</h6>
+    <iframe id='api-explorer' src='http://localhost:3000/explorer/' />
   </div>
 </template>
 
 <script>
-export default {}
+import iFrameResize from 'iframe-resizer'
+export default {
+  mounted: async function() {
+    $('#api-explorer').iFrameResize({
+      log: false
+    })
+  },
+}
 </script>
 
 <style scoped>
+iframe {
+  min-width: 100%;
+  width: 1px;
+  border: none;
+}
 </style>
